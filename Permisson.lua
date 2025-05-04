c141515b78d64bc32e00e2d10800a393393124c0
Marcos42334534 <felipe89731@gmail.com>
2025-05-03 23:31:56 -0300

    Update and rename README.md to Permisson.lua

---
+++ Permisson.lua
@@ -0,0 +1,19 @@
+local Tabela = {
+	Races = {
+		["Shadow Monarc"] = {PlayersPermission = 
+			{
+				1449978472;
+			}
+			,Icon = 97934617652188};
+		
+		["Anjo"] = {PlayersPermission = 
+			{
+				2267845183;
+
+			}
+			,Icon = 81599403581004};
+	}
+}
+
+
+return Tabela
