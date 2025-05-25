5f14888d2c9aba6a0eb1aedb7b87c2ee0bbdae75
Marcos42334534 <felipe89731@gmail.com>
2025-05-25 15:55:56 -0300

    Update Permisson.lua

--- Permisson.lua
+++ Permisson.lua
@@ -3,12 +3,14 @@ local Tabela = {
 		["Shadow Monarc"] = {PlayersPermission = 
 			{
 				1449978472;
+				8345352716;
 			}
 			,Icon = 97934617652188};
 		
 		["Anjo"] = {PlayersPermission = 
 			{
 				2267845183;
+				8345352716;
 
 			}
 			,Icon = 81599403581004};
