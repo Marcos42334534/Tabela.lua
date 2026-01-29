8efadc0fc5fc961a3e7b89b0238ba0589ed5f994
Marcos42334534 <felipe89731@gmail.com>
2026-01-29 00:50:13 -0300

    Add player permissions for Races in Permisson.lua

--- Permisson.lua
+++ Permisson.lua
@@ -1,1 +1,20 @@
-a
+local Tabela = {
+	Races = {
+		["Shadow Monarc"] = {PlayersPermission = 
+			{
+				1449978472;
+			}
+			,Icon = 97934617652188};
+		
+		["Anjo"] = {PlayersPermission = 
+			{
+				2267845183;
+				2948418577;
+				
+			}
+			,Icon = 81599403581004};
+	}
+}
+
+
+return Tabela
